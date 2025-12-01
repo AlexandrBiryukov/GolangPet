953bdf1e100b97deb915bdbd5ab4730fd9c5950c
Alex <alex.biryukov99@gmail.com>
2025-12-01 10:14:58 +0300

    проверочка на наличие поля task

--- Pet/main.go
+++ Pet/main.go
@@ -22,14 +22,30 @@ type requestBody struct {
 func handleTask(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
+		var raw map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&raw)
+		if err != nil {
+			http.Error(w, "Неправильный json", http.StatusBadRequest)
+			return
+		}
+
+		_, ok := raw["task"]
+		if !ok {
+			http.Error(w, "Нет поля task", http.StatusBadRequest)
+			return
+		}
+
 		var body requestBody
-		json.NewDecoder(r.Body).Decode(&body)
+		rawBytes, _ := json.Marshal(raw)
+		json.Unmarshal(rawBytes, &body)
+
+		w.Write(([]byte)("Запрос выполнен"))
 		task = body.Task
 		return
 	}
 
 	if r.Method == http.MethodGet {
-		w.Write(([]byte)("hello, " + task))
+		w.Write(([]byte)("Hello, " + task))
 		return
 	}
 
